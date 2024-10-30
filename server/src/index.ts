922d99f87f15e5fd6b74866ecc6542f886c6ace1
KillivalavanE <ekilli2003@gmail.com>
2024-10-30 09:35:10 +0530

    new file:   .gitignore 	new file:   server/ecosystem.config.js 	modified:   server/src/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,8 +19,8 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(cors())
 
 
-const port = process.env.PORT || 3000
-app.listen(port,()=>{
+const port = Number(process.env.PORT) || 3000
+app.listen(port,"0.0.0.0",()=>{
     console.log(`Server running at port ${port}`)
 })
 
